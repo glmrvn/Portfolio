dd3e079813794bdc4f23ac5e9b8f6f8cc935f6f3
glmrvn <glmrvn@gmail.com>
2020-11-25 15:54:18 +0300

    Fixed bottom links hover

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function chbg(color) {
   link = document.getElementsByClassName('link')
   for(var i = 0, j = link.length; i < j; i++) { 
-    link[i].style.color = color;
+    link[i].style.textDecoration = color;
   }
 }
