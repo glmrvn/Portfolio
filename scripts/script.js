f1b3237deeee3cfe0ae6a61e96a6413aa7911215
Alex Daykov <glmrvn@gmail.com>
2021-08-07 16:52:23 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,3 @@ function changeOpacity(value) {
     link[i].style.opacity = value;
   }
 }
-
-if (typeof twttr === "undefined") {
-  <![CDATA[
- !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-  ]]>
-}
